0c68ed4a2b7bd535d6f2fcbeaa7cffe22977c510
RajatSharma07 <rajat.sharma@xebia.com>
2024-09-10 01:04:54 +0530

    documentation completed

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -5,18 +5,22 @@ const useSocket = (url: string) => {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
+    // This will set connection with the server.
     const socketIo = io(url);
 
+    // whenever our socket is connected this message will get printed in console
     socketIo.on('connect', () => {
       console.log('Connected to the server');
     });
 
+    // whenever our socket is disconnected this message will get printed in console
     socketIo.on('message', (data) => {
       console.log('Message from server:', data);
     });
 
     setSocket(socketIo);
 
+    // This useEffect ensures that whenever the page is removed from the dom (unmounted) then socket will automatically gets disconnected.
     return () => {
       socketIo.disconnect();
     };
